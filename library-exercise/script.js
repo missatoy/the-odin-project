fdec0377f70cc6539f0ae0361546e6ef42724c69
Missato Yamashita <rmissato@gmail.com>
2022-09-22 12:36:34 +0200

    Created book object constructor with separate function using prototype properties

--- library-exercise/script.js
+++ library-exercise/script.js
@@ -1,9 +1,20 @@
+// function Book(title, author, pages, info) {
+//   this.title = title
+//   this.author = author
+//   this.pages = pages
+//   this.info = info
+//   this.sayInfo = function() {
+//     return info
+//   }
+// }
+
 function Book(title, author, pages, info) {
   this.title = title
   this.author = author
   this.pages = pages
   this.info = info
-  this.sayInfo = function() {
-    return info
-  }
+}
+
+Book.prototype.readInfo = function() {
+  return this.info
 }
