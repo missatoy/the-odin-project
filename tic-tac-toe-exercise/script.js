89093e7e18a15a4923e122640fba4498eb05bda7
Missato Yamashita <rmissato@gmail.com>
2022-09-27 15:01:42 +0200

    added a prompt for user's imput, added a counter for computer and user

--- tic-tac-toe-exercise/script.js
+++ tic-tac-toe-exercise/script.js
@@ -1,26 +1,35 @@
-const options = ["Rock", "Paper", "Scissor"]
+const tools = ["Rock", "Paper", "Scissor"]
 
-let computerChoice = options[Math.floor(Math.random()*options.length)]
+let computerChoice = tools[Math.floor(Math.random()*tools.length)]
 
 function getComputerChoice() {
   return computerChoice
 }
 
+let playerScore = 0
+let computerScore = 0
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === "Rock" && computerSelection === "Scissor" ||
-  playerSelection === "Paper" && computerSelection === "Rock" ||
-  playerSelection === "Scissor" && computerSelection === "Paper") {
+      playerSelection === "Paper" && computerSelection === "Rock" ||
+      playerSelection === "Scissor" && computerSelection === "Paper") {
+    playerScore++
     return `Well done! ${playerSelection} beats ${computerSelection}`
   } else if (playerSelection === computerSelection) {
     return "Oh no, it's a tie!"
   } else {
+    computerScore++
     return `Oh no! You lose, ${computerSelection} beats ${playerSelection}`
   }
 }
 
-const playerSelection = "Rock"
+const playerSelection = prompt("Pick a tool")
 const computerSelection = getComputerChoice()
 console.log(playRound(playerSelection, computerSelection))
 
 console.log(`Computer: ${getComputerChoice()}`)
 console.log(`Player: ${playerSelection}`)
+console.log(`Computer ${computerScore} - ${playerScore} Player`)
+
+function game() {
+}
