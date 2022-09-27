21dbd9af7402ca74ed10af9835188876b604bda5
Missato Yamashita <rmissato@gmail.com>
2022-09-27 14:17:22 +0200

    made coditions more accurate

--- tic-tac-toe-exercise/script.js
+++ tic-tac-toe-exercise/script.js
@@ -1,5 +1,3 @@
-console.log("Hello world!")
-
 const options = ["Rock", "Paper", "Scissor"]
 
 let computerChoice = options[Math.floor(Math.random()*options.length)]
@@ -8,14 +6,21 @@ function getComputerChoice() {
   return computerChoice
 }
 
-console.log(getComputerChoice())
-
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === "Rock" && computerSelection === "Scissor" ||
-      playerSelection === "Paper" && computerSelection === "Rock" ||
-      playerSelection === "Scissor" && computerSelection === "Paper") {
+  playerSelection === "Paper" && computerSelection === "Rock" ||
+  playerSelection === "Scissor" && computerSelection === "Paper") {
     return `Well done! ${playerSelection} beats ${computerSelection}`
+  } else if (playerSelection === computerSelection) {
+    return "Oh no, it's a tie!"
   } else {
     return `Oh no! You lose, ${computerSelection} beats ${playerSelection}`
   }
 }
+
+const playerSelection = "Rock"
+const computerSelection = getComputerChoice()
+console.log(playRound(playerSelection, computerSelection))
+
+console.log(`Computer: ${getComputerChoice()}`)
+console.log(`Player: ${playerSelection}`)
