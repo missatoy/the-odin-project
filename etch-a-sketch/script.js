2fb758914dde64e1d9b61a0f5c514359383534cd
Missato Yamashita <rmissato@gmail.com>
2022-10-11 15:28:44 +0200

    still figuring out how to insert divs through js

---
+++ etch-a-sketch/script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector('#container');
+const times = 16
+let counter = 0
+
+// // for(let i = 0; i < times; i++) {
+// //   container.innerHTML = "<div class='d-block d-sm-inline-block p-4 bg-primary'></div>";
+// //   console.log(counter += 1)
+// //   // container.innerHTML = "<div class='w-100'></div>";
+// // }
+
+
+container.innerHTML =
+"<div class='d-block d-sm-inline-block p-4 bg-primary'></div>" +
+"<div class='d-block d-sm-inline-block p-4 bg-primary'></div>";
+
