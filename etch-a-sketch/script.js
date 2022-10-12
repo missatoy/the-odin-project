a58321e82fd57146c784b123a6ed00b2c3f63ebd
Missato Yamashita <rmissato@gmail.com>
2022-10-12 15:08:51 +0200

    implemented ratio bar and assigned values. Now is possible to reratio the board with the bar on frontend

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,10 +1,12 @@
-// const board = document.getElementById('board') ???
+const board = document.getElementById('board')
+const ratioBar = document.getElementById('ratio-bar')
 
 const times = 16;
 
 function makeGrid(rows, cols) {
   board.style.setProperty('--grid-rows', rows);
   board.style.setProperty('--grid-cols', cols);
+  board.innerHTML = '' // Cleaning the board so it doesn't accumulate
   for ( i = 0; i < (rows * cols); i++) {
     let cell = document.createElement('div');
     cell.classList.add('grid-item');
@@ -14,15 +16,33 @@ function makeGrid(rows, cols) {
   }
 }
 
+makeGrid(26, 26);
+
+// Press and hold
 let mouseClick = false;
 document.body.onmousedown = () => (mouseClick = true);
 document.body.onmouseup = () => (mouseClick = false);
 
+
 function changeCellColor(e) {
-  // console.log(e)
   if (e.type === "mouseover" && mouseClick === true) {
     e.target.style.backgroundColor = 'pink'
   }
 }
 
-makeGrid(16, 16);
+ratioBar.addEventListener('click', changeRatio)
+
+
+function changeRatio() {
+  if (ratioBar.value === "1") {
+    makeGrid(16,16)
+  } else if (ratioBar.value === "2") {
+    makeGrid(21,21)
+  } else if (ratioBar.value === "3") {
+    makeGrid(26,26)
+  } else if (ratioBar.value === "4") {
+    makeGrid(31,31)
+  } else if (ratioBar.value === "5") {
+    makeGrid(36,36)
+  }
+}
